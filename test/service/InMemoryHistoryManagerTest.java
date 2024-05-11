3929165e3c8f64f9875505b1cdca636da5bb31a8
rzhao <dns.mail@mail.ru>
2024-05-11 16:03:57 +0700

    added tests to InMemoryHistoryManagerTest

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -1,31 +1,38 @@
 package service;
 
 import model.Epic;
-import model.Status;
 import model.SubTask;
 import model.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collections;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InMemoryHistoryManagerTest {
+    HistoryManager historyManager;
     InMemoryTaskManager memoryTaskManager;
-    private Task task1;
-    private Epic epic;
-    private SubTask subTask;
+
 
     @BeforeEach
     void init() {
-        HistoryManager historyManager = new InMemoryHistoryManager();
+        historyManager = new InMemoryHistoryManager();
         memoryTaskManager = new InMemoryTaskManager(historyManager);
-        task1 = memoryTaskManager.create(new Task("new task", "desc"));
-        epic = memoryTaskManager.createEpic(new Epic("new epic", "desc"));
-        subTask = memoryTaskManager.createSubTask(new SubTask("new subTask", "desc", epic));
+        memoryTaskManager.create(new Task("new task", "desc"));
+        Epic epic = memoryTaskManager.createEpic(new Epic("new epic", "desc"));
+        memoryTaskManager.createSubTask(new SubTask("new subTask", "desc", epic));
     }
 
+    @Test
+    @DisplayName("Должен добавить и получить список из 10 последних просмотренных задач, эпиков, подзадач")
+    void shouldAddToHistory() {
+        for (int i = 0; i < 4; i++) {
+            historyManager.add(memoryTaskManager.get(0));
+            historyManager.add(memoryTaskManager.getEpic(1));
+            historyManager.add(memoryTaskManager.getSubTask(2));
+        }
+        int countHistoryExpected = historyManager.getAll().size();
+        int countActual = 10;
+        assertEquals(countHistoryExpected, countActual, "Должен добавить и получить список последних просмотренных 10 задач, эпиков, подзадач");
+    }
 }
